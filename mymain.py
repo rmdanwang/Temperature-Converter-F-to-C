096dcd9808572cceee387ef23c7a7b0213c5a8e8
Dan Wang <rm.danwang@gmail.com>
2019-07-04 16:57:24 -0500

    Add files via upload

---
+++ mymain.py
@@ -0,0 +1,11 @@
+# Homework by Ru Meng(Dan) Wang mymain.py
+import temperatureConverter
+
+F = input("Enter degree in fahrenheit to be converted to celsius here: ")
+result = temperatureConverter.fahrenheitToCelsius(F)
+print(result)
+
+
+C = input("Enter degree in celsius to be converted to fahrenheit here: ")
+result = temperatureConverter.celsiusToFahrenheit(C)
+print(result)
---
+++ temperatureConverter.py
@@ -0,0 +1,11 @@
+# Homework by Ru Meng(Dan) Wang temperatureConverter.py
+
+def fahrenheitToCelsius(tempInFahrenheit):
+    return(float(tempInFahrenheit)- 32)* 5/9
+
+    #print(tempInFahrenheit, "degrees Fahrenheit equals to", Celsius, "degrees in Celsius") this is not necessary anymore
+
+def celsiusToFahrenheit(tempInCelsius):
+    return(9 / 5 * float(tempInCelsius)) + 32
+
+    #print(tempInCelsius, "degrees Celsius equals to", Fahrenheit, "degrees in fahrenheit")this is not necessary anymore
